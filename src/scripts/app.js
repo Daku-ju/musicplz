0cd9dea960c649c84b3adfb6708c7bf226e6817b
Julie Corbeel <julie.corbeel@gmail.com>
2022-11-09 18:18:31 +0100

    ajout slider plus changement dans le css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,58 @@
-console.info('Hello world');
+"use strict";
+
+const btnPrev = document.querySelector(".slider__btn--prev"),
+      btnNext = document.querySelector(".slider__btn--next");
+
+btnNext.addEventListener("click", next);
+
+btnPrev.addEventListener("click", prev);
+
+//touche clavier
+document.addEventListener("keydown", function(e){
+    if(e.code == "ArrowLeft"){
+        prev();
+    }
+    else if(e.code == "ArrowRight"){
+        next();
+    }
+})
+
+//hammer
+const slider = document.querySelector(".slider"),
+    hammerSlider = new Hammer(slider);
+    hammerSlider.on("swipeleft", next);
+    hammerSlider.on("swiperight", prev);
+
+function next(){
+
+    let elShow = document.querySelector(".slider__el--show"),
+        elNext = elShow.nextElementSibling;
+    
+    elShow.classList.remove("slider__el--show")
+
+    if(elNext){
+        elNext.classList.add("slider__el--show");
+    }
+    else{
+        let elFirst = elShow.parentNode.firstElementChild;
+        elFirst.classList.add("slider__el--show");
+    }
+
+}
+
+function prev(){
+
+    let elShow = document.querySelector(".slider__el--show"),
+        elPrev = elShow.previousElementSibling;
+    
+    elShow.classList.remove("slider__el--show")
+
+    if(elPrev){
+        elPrev.classList.add("slider__el--show");
+    }
+    else{
+        let elLast = elShow.parentNode.lastElementChild;
+        elLast.classList.add("slider__el--show");
+    }
+
+}
